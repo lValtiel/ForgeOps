c666430fd8d84da62d97adec87b0bf9945a6f2e6
lValtiel <dev.emersonc@gmail.com>
2026-08-24 01:18:43 -0400

    first commit

---
+++ src/main/java/com/valtiel/forgeOps/ForgeOpsApplication.java
@@ -0,0 +1,13 @@
+package com.valtiel.forgeOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgeOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valtiel/forgeOps/ForgeOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valtiel.forgeOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgeOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
